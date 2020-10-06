698a0f6057ed4edc6a05db81f2966fb9788ceb3f
Relham <mohua@192-168-1-102.tpgi.com.au>
2020-10-07 09:37:47 +1100

    Updates

---
+++ precache-manifest.22ab4d3b259b59ea37ed5029ddefc2ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7ed8325065ff7ea3d3695e73647de1",
+    "url": "/players/index.html"
+  },
+  {
+    "revision": "2826a0e81d791f475d79",
+    "url": "/players/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4e77c3c04c3d421e097e",
+    "url": "/players/static/css/main.821c2863.chunk.css"
+  },
+  {
+    "revision": "2826a0e81d791f475d79",
+    "url": "/players/static/js/2.0a1616d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/players/static/js/2.0a1616d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e77c3c04c3d421e097e",
+    "url": "/players/static/js/main.4811ee7e.chunk.js"
+  },
+  {
+    "revision": "fc86e0b2a3d1ca320bd8",
+    "url": "/players/static/js/runtime-main.023b825a.js"
+  },
+  {
+    "revision": "b6e9bb37f4e5cd1059e6ebb846a6d40e",
+    "url": "/players/static/media/SEGA LOGO FONT.b6e9bb37.woff"
+  }
+]);
